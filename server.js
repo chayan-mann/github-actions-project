bdfa6f25fff0fc87bd1c68abbfe807eda7f2b2f5
chayan-mann <chayanmann09@gmail.com>
2025-10-25 00:13:54 +0530

    Refactor server response message in server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-// const express = require("express");
-// const app = express();
-// const PORT = process.env.PORT || 8000;
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Hey from GitHub Actions CI/CD!" });
-// });
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+  res.json({ message: "Hello from GitHub Actions CI/CD!" });
 });
 
 module.exports = app;
